6360d8b46e674a18a4f6035a182efbd96c4ea11d
katresars <katresars@example.com>
2024-05-09 01:13:42 +0800

    test

--- src/pages/streams/index.tsx
+++ src/pages/streams/index.tsx
@@ -6,6 +6,7 @@ const Streams: NextPage = () => {
     <main className="flex-1 p-6">
       <p>This is the streams page</p>
       <hr className="my-2" />
+      <p>This is the streams page test</p>
 
       {/* <Trash2Icon className='absolute top-2 right-2 opacity-0 group-hover:opacity-100 text-white'  */}
                 {/* onClick={buttonFunction}/> */}
